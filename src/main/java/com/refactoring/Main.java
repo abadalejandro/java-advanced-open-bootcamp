46481b414037a04e12c08266b69a505d7cde1bcb
abadalejandro <36165616+abadalejandro@users.noreply.github.com>
2022-10-09 18:02:25 +0200

    refactoring with reduce

--- src/main/java/com/refactoring/Main.java
+++ src/main/java/com/refactoring/Main.java
@@ -1,20 +1,17 @@
 package com.refactoring;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        int[] numeros = {12, 23, 45, 1, 9, 0};
+        int[] numeros = {12, 23, 45, 1, 9, 0, -4};
 
         imprimeMayorYMenor(numeros);
     }
 
     public static void imprimeMayorYMenor(int[] numeros) {
-        int mayor = 0;
-        int menor = numeros[0];
-
-        for (int numero : numeros) {
-            mayor = mayor < numero ? numero : mayor;
-            menor = menor > numero ? numero : menor;
-        }
+        int mayor = Arrays.stream(numeros).reduce(0, (a, b) -> (a > b) ? a : b );
+        int menor = Arrays.stream(numeros).reduce(0, (a, b) -> (a < b) ? a : b );
 
         System.out.println("Mayor: " + mayor + " Menor: " + menor);
     }
