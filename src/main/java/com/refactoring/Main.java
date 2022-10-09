90981a8968f156b88c7757ceb03e46e38c061732
abadalejandro <36165616+abadalejandro@users.noreply.github.com>
2022-10-09 18:07:24 +0200

    added ifAnidado method

--- src/main/java/com/refactoring/Main.java
+++ src/main/java/com/refactoring/Main.java
@@ -16,5 +16,14 @@ public class Main {
         System.out.println("Mayor: " + mayor + " Menor: " + menor);
     }
 
+    public static int ifAnidado1(){
+        int valor1 = 10;
+        int valor2 = 10;
+        if(valor1 > valor2){
+            return valor1;
+        }else{
+            return valor2;
+        }
+    }
 
 }
